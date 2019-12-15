3fe8ab1dc922ff2fc93d2ebfe43e1961ca581cd9
ifurdey2 <ifurdey2@gmail.com>
2019-12-15 22:36:12 +0100

    Update Tlist.h

--- Tlist.h
+++ Tlist.h
@@ -26,4 +26,18 @@ typedef struct List {            //2
     node* last;
 } Tlist;
 
+void push_back(Tlist* l, realty val) { // ‚ÒÚ‡‚ÎˇÂÚ ˝ÎÂÏÂÌÚ ‚ ÍÓÌÂˆ 3
+    node* new = malloc(sizeof(node));
+    new->value = val;
+    new->next = NULL;
+
+    if (l->first == NULL) { // ÂÒÎË ÒÔËÒÓÍ ÔÛÒÚÓÈ
+        l->first = l->last = new;
+    }
+    else {
+        l->last->next = new;
+        l->last = new;
+    }
+};
+
 #endif // TLIST_H_INCLUDED
