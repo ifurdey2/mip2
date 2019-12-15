974d4de23dde5fabc61c999e41aefd6fb84f7270
ifurdey2 <ifurdey2@gmail.com>
2019-12-15 22:34:22 +0100

    Update Tlist.h

--- Tlist.h
+++ Tlist.h
@@ -7,5 +7,14 @@
 #include <stdlib.h>
 #define FILENAME "reality.txt"
 
+typedef struct {
+  char  Category[50];
+  char  Location[50];
+  char  Street[100];
+  int  Square;
+  int  Price;
+  char  Description[200];
+}realty;
+
 
 #endif // TLIST_H_INCLUDED
