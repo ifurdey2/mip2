2164b866d7010f4897eceb06fb29a6875a220d0e
ifurdey2 <ifurdey2@gmail.com>
2019-12-15 22:51:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,6 +14,35 @@ int main()
        scanf("%c",&ch);
        while (getchar() != '\n');
        switch(ch){
+               case 'n':{
+                  n=loadformfile(&L);
+                  printf("Nacitalo sa %d\n zaznamov",n);
+                  break;
+               }
+               case 'v':{
+                  print_All_List(L);
+                  break;
+               }
+               case 'k':{
+                  free_list(&L);
+                  return 0;
+               }
+               case 'p':{
+                   printf("Position: ");
+                   scanf("%d",&n);
+                   gets(buf.Category);
+                   gets(buf.Location);
+                   gets(buf.Street);
+                   gets(str);
+                   buf.Square=atoi(str);
+                   gets(str);
+                   buf.Price=atoi(str);
+                   gets(buf.Description);
+                   while (getchar() != '\n');
+                   insert_list(&L,n,buf);
+                  break;
+               }
+               
                
     return 0;
 }
