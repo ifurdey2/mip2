02a470db42d8fbe361ebbbf8944d16eca20980f3
ifurdey2 <ifurdey2@gmail.com>
2019-12-15 22:48:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 #define MAXLENSTR (255)
 int main()
 {
-   
+   Tlist L={NULL,NULL};
+   char ch;
+   int n;
+   realty buf;
+   char str[MAXLENSTR];
+   while (1){
+       printf("> ");
+       scanf("%c",&ch);
+       while (getchar() != '\n');
+       switch(ch){
+               
     return 0;
 }
