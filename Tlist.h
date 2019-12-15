954e0423a007c72627576bebe08ab54c79178d0d
ifurdey2 <ifurdey2@gmail.com>
2019-12-15 22:31:27 +0100

    Update Tlist.h

--- Tlist.h
+++ Tlist.h
@@ -1,1 +1,11 @@
+#ifndef TLIST_H_INCLUDED
+#define TLIST_H_INCLUDED
 
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define FILENAME "reality.txt"
+
+
+#endif // TLIST_H_INCLUDED
