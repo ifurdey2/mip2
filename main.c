4fb42186e0eda8c2cb8cbc663adff10e3cf411d3
ifurdey2 <ifurdey2@gmail.com>
2019-12-15 22:51:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -42,6 +42,38 @@ int main()
                    insert_list(&L,n,buf);
                   break;
                }
+               case 'z':{
+                  printf("Location to delete entries: ");
+                  gets(str);
+                  while (getchar() != '\n');
+                  n=delete_from_list(&L,str);
+                  printf("Vymazalo sa %d zaznamov\n",n);
+                  break;
+               }
+               case 'h':{
+                   printf("V ponuke su len reality s vyssou cenou ");
+                   scanf("%d",&n);
+                   while (getchar() != '\n');
+                   find_list(L,n);
+                   break;
+               }
+               case 'a':{
+                  printf("Location for changing data: ");
+                  while (getchar() != '\n');
+                  gets(str);
+                  gets(buf.Category);
+                  gets(buf.Location);
+                  gets(buf.Street);
+                  gets(str);
+                  buf.Square=atoi(str);
+                  gets(str);
+                  buf.Price=atoi(str);
+                  gets(buf.Description);
+                  while (getchar() != '\n');
+                  n=chenge_list(&L,str,buf);
+                  printf("Aktualizovalo sa %d zaznamov\n",n);
+                  break;
+               }
                
                
     return 0;
